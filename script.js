59878b0f271a5b2035c01cba14f062064357d886
pepashumareva <pepa.shumareva@mentormate.com>
2022-10-27 15:47:50 +0300

    Refactoring issue

--- script.js
+++ script.js
@@ -12,8 +12,13 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
 //Starting conditions
 const restart = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
   score0El.textContent = 0;
   score1El.textContent = 0;
   current0El.textContent = 0;
@@ -24,11 +29,6 @@ const restart = function () {
     .querySelector(`.player--${activePlayer}`)
     .classList.remove('player--winner');
 };
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
